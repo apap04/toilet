50371103c620092966a3318d09b6c0a227500ed6
Andreas P <me@apap04.com>
2019-08-22 17:50:22 -0400

    REE
    Signed-off-by: Andreas P <me@apap04.com>

---
+++ cogs/test.py
@@ -0,0 +1,13 @@
+import discord
+from discord.ext import commands
+
+class Test(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(name="test")
+    async def test(self, ctx, arg):
+        await ctx.send(arg)
+
+def setup(bot):
+    bot.add_cog(Test(bot))
--- index.py
+++ index.py
@@ -1,9 +1,32 @@
+__author__ = "apap04#3355"
 import discord
+from discord.ext import commands
+import os
+import logging
+import random
+
+doken = "NjE0MTQzODUwODQ3NTM1MTE0.XV7MMQ.GBg_D1OqCE-ClMeCOoEGlAztCb8" # dev token.
+quotes = ["reee", "ready to flush owo", "poop", "poop funny!", "OwO what's this?"]
 
 client = discord.Client()
+bot = commands.Bot(command_prefix="toilet ", description="the bathroom utility (toilet paper not included)", owner_id="138056116880932864")
 
-@client.event
+@bot.event
 async def on_ready():
-    print("ready to flush ;)\n")
+    print("\n" + random.choice(quotes) + "\n")
 
-client.run('NjE0MTE1MjIxOTY3MDc3Mzg2.XV6xZQ.G6TPwqKKG6Z6Fx_TfSqeoZwhxoU')
+@bot.command(is_owner)
+async def load(ctx, extension):
+    bot.load_extension(f'cogs.{extension}')
+
+
+@bot.command(is_owner)
+async def unload(ctx, extension):
+    bot.unload_extension(f'cogs.{extension}')
+
+for filename in os.listdir('./cogs'):
+    if filename.endswith('.py'):
+        bot.load_extension(f'cogs.{filename[:-3]}')
+
+
+bot.run(doken)
