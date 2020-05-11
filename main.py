da06be22eecfbbb9b1b1eb4b37dc0eb7285b8ad6
Andreas P <andreas@apap04.com>
2020-05-11 07:35:25 +0000

    bot: remove cmd parser
    this is no longer needed

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
 import os
 import random
-import argparse
 
 from utils import default
 from utils.data import Bot, HelpFormat
 
-parser = argparse.ArgumentParser(description='le epic bot everyone will like.')
-parser.add_argument("--log", choices=["debug", "dinfo", "info"], help="specific logging flags. this will change game and token values!!")
 quotes = ["reee", "ready to flush owo", "poop", "poop funny!", "OwO what's this?", "h", "um :flushed:", "The token is 5."]
 
 config = default.get("config.json")
