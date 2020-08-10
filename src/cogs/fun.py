35e0096c1083a7e29c21b21d784e114f9ea0536e
Andreas Papacharalampous <andreas@apap04.com>
2020-08-10 03:53:02 -0400

    fun(user): set id footer
    Signed-off-by: Andreas Papacharalampous <andreas@apap04.com>

--- src/cogs/fun.py
+++ src/cogs/fun.py
@@ -94,11 +94,12 @@ class Fun(commands.Cog):
     async def get_user_info(self, ctx, member: discord.Member):
         try:
             embed = discord.Embed(colour=discord.Colour(0xff55ff), description=f"{member.mention}")
-            embed.set_author(name=f"{member.display_name}#{member.discriminator}", icon_url=member.avatar_url)
+            embed.set_author(name=str(member), icon_url=member.avatar_url)
             embed.set_thumbnail(url=member.avatar_url)
             embed.add_field(name="Join date", value=f"{member.joined_at}"[0:10])
             embed.add_field(name="Creation date", value=f"{member.created_at}"[0:10])
-            embed.add_field(name="Roles", value=f"{discord.Role.mention}", inline=True)
+            # embed.add_field(name="Roles", value=str(discord.Member.roles), inline=True)
+            embed.set_footer(text="ID: " + str(member.id))
             await ctx.send(embed=embed)
         except Exception as e:
             await ctx.send(e)
